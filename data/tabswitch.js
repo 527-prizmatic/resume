c6fa7c1aa7fbd7aea7ee9e9d943adbedac45d39d
Prizmatic <66261352+527-prizmatic@users.noreply.github.com>
2022-01-19 22:30:14 +0100

    Update tabswitch.js

--- data/tabswitch.js
+++ data/tabswitch.js
@@ -1,3 +1,8 @@
 function myFunction() {
-  document.getElementById("main-info").innerHTML = "pavouka";
+	document.getElementById("main-info").fadeOut(1000, pok());
+}
+
+function pok() {
+	document.getElementById("main-info").innerHTML = "pavouka";
+	document.getElementById("main-info").fadeIn(1000);
 }
