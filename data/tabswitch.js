6ab9e310fc6fd3d44bd285a92851883871a37803
Prizmatic <66261352+527-prizmatic@users.noreply.github.com>
2022-01-20 20:55:54 +0100

    Delete tabswitch.js

--- data/tabswitch.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-	document.getElementById("main-info").fadeOut(1000, pok());
-}
-
-function pok() {
-	document.getElementById("main-info").innerHTML = "pavouka";
-	document.getElementById("main-info").fadeIn(1000);
-}
